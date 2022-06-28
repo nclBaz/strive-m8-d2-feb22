dcc40e217f299ce609d48838fe889fd36f77de10
Riccardo Gulin <riccardo@nucleode.com>
2022-06-28 10:18:02 +0200

    Adds server stuff; adds Users endpoints

---
+++ src/api/users/model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
+
+const UserSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["User", "Admin"], default: "User" },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default model("User", UserSchema)
